9551adcb65e4ca38ab3bcc06192d5cd345cf52d7
Maxx730 <max.kinghorn@gmail.com>
2013-10-11 16:12:04 -0400

    Began work on the search object which will include functionality to search globally or specified to a particular object.
    Signed-off-by: Maxx730 <max.kinghorn@gmail.com>

--- sim_admin/includes/simpl_search.php
+++ sim_admin/includes/simpl_search.php
@@ -2,19 +2,31 @@
 
 //search object that will find and display information all objects for display
 class simpl_search{
+
+	public $term;
+	public $username;
 	
-	public function __construct(){
-
-
+	//this function takes in a term as well as a user object for search results.
+	public function __construct($term,$user){
+		$this->term = $term;
+		$this->username = $user->return_name();
 	}
 
 	//this function will search throughout pages,tags,users etc for the specified search term.
 	public function global_search($term){
-
+		$sql_pages = "SELECT * FROM pages";
+		$sql_users = "SELECT * FROM users";
+		$sql_tags = "SELECT * FROM subjects";
+		$sql_investigators = "SELECT * FROM investigators";
 	}
 
 	//search function for specific objects, such as pages, tages, users etc.
 	public function specified_search($cat,$term){
+
+	}
+
+	//depending on the scope of the search build a list of objects that relate to the search term, meaning that if 
+	public function build_results($scope){
 
 	}
 
